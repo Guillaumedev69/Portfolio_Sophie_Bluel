93b4fed564b19dde29904be01d18da8e6da66a8a
Guillaume Destree <devstree.guillaume@gmail.com>
2024-02-16 14:38:15 +0100

    Creation de la semantique de base du body et ajout des href aux li du nav

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,4 +1,6 @@
 const header = document.createElement("header") // Creation du header
+const main = document.createElement("main") // Creation du main
+const footer = document.createElement("footer") // Creation du footer
 
 const h1 = document.createElement("h1") //Creation du h1
 h1.innerText = "Sophie Bluel"
@@ -8,23 +10,33 @@ h1.appendChild(spanH1) // Ajout du span au h1
 
 const nav = document.createElement("nav") // Creation du nav + ul + li
 const ulNav = document.createElement("ul")
-const items = ["projet", "contact","login"]
+const link = ["projet", "contact","login"]
+const linkA = ["index.html#portfolio","index.html#contact","login.html"]
+
 const iconIstagram = document.createElement("img") // Integration logo insta
 iconIstagram.src = "assets/icons/instagram.png"
 iconIstagram.alt = "logo-Instagram"
 const liInstagram = document.createElement("li") // Ajout du logo dans les li
-items.forEach(function (itemText) {
+const aInstagram = document.createElement("a")
+aInstagram.href = "https://www.instagram.com/"
+
+link.forEach(function (linkText, indexA) { // Creation des li via items
     const li = document.createElement("li")
     const a = document.createElement ("a")
-    a.textContent = itemText
+    a.textContent = linkText 
+    a.href = linkA[indexA] // liaison des link aux linkA
     li.appendChild(a)
     ulNav.appendChild(li)
 })
 
-liInstagram.appendChild(iconIstagram) // Ajout du logo dans les li
+aInstagram.appendChild(iconIstagram) // lien entre a href et iconIstagram
+liInstagram.appendChild(aInstagram) // Ajout du logo dans les li
 ulNav.appendChild(liInstagram) // Ajout du logo dans ul
 nav.appendChild(ulNav)
 header.appendChild(h1)
 header.appendChild(nav)
 
-document.body.insertBefore(header, document.body.firstChild)
+// Appel semantique principale du body
+document.body.appendChild(header);
+document.body.appendChild(main);
+document.body.appendChild(footer);
