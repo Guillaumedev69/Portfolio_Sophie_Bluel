8e0f6c7e2c78efa1070ab0fa62c69fae554a0f60
Guillaume Destree <devstree.guillaume@gmail.com>
2024-02-18 00:08:40 +0100

    ajout des ; manquants

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,5 +1,5 @@
-const swaggerWorks = await fetch("http://localhost:5678/api/works")
-const works = await swaggerWorks.json()
+const swaggerWorks = await fetch("http://localhost:5678/api/works");
+const works = await swaggerWorks.json();
 
 function genererWorks(works) {
     const sectionGallery = document.querySelector("#portfolio");
@@ -16,65 +16,64 @@ function genererWorks(works) {
 
 
     for (let i = 0; i < works.length; i++) {
-        const figureGallery = works[i]
-        const worksElement = document.createElement("figure")
-        const imageElement = document.createElement("img")
-            imageElement.src = figureGallery.imageUrl
-        const nomElement = document.createElement("figcaption")
-            nomElement.innerText = figureGallery.title
-        worksElement.dataset.categoryId = figureGallery.categoryId // Recuperation des categories des works
-        worksElement.appendChild(imageElement)
-        worksElement.appendChild(nomElement)
-        divGallery.appendChild(worksElement)
+        const figureGallery = works[i];
+        const worksElement = document.createElement("figure");
+        const imageElement = document.createElement("img");
+            imageElement.src = figureGallery.imageUrl;
+        const nomElement = document.createElement("figcaption");
+            nomElement.innerText = figureGallery.title;
+        worksElement.dataset.categoryId = figureGallery.categoryId; // Recuperation des categories des works
+        worksElement.appendChild(imageElement);
+        worksElement.appendChild(nomElement);
+        divGallery.appendChild(worksElement);
     }
     sectionGallery.appendChild(divGallery);
 }
 
-const swaggerCategories = await fetch("http://localhost:5678/api/categories")
-const categories = await swaggerCategories.json()
+const swaggerCategories = await fetch("http://localhost:5678/api/categories");
+const categories = await swaggerCategories.json();
 
 function genererButton(categories) {
-    const sectionFiltres = document.querySelector("#portfolio")
-    const divFiltres = document.createElement("div")
-    divFiltres.className = "btn-filtres"
+    const sectionFiltres = document.querySelector("#portfolio");
+    const divFiltres = document.createElement("div");
+    divFiltres.className = "btn-filtres";
 
-    const boutonTous = document.createElement("button")
-    boutonTous.className = "filtres-tous"
-    const nomBoutonTous = document.createElement("h3")
-    nomBoutonTous.innerText = "Tous"
+    const boutonTous = document.createElement("button");
+    boutonTous.className = "filtres-tous";
+    const nomBoutonTous = document.createElement("h3");
+    nomBoutonTous.innerText = "Tous";
     
-    sectionFiltres.appendChild(divFiltres)
-    divFiltres.appendChild(boutonTous)
-    boutonTous.appendChild(nomBoutonTous)
+    sectionFiltres.appendChild(divFiltres);
+    divFiltres.appendChild(boutonTous);
+    boutonTous.appendChild(nomBoutonTous);
 
     boutonTous.addEventListener("click", function () {
-        document.querySelector(".gallery").innerHTML = ""
-        genererWorks(works)
-    })
-    
+        document.querySelector(".gallery").innerHTML = "";
+        genererWorks(works);
+    });
 
     for (let i = 0; i < categories.length; i++) {
-        const filtresGallery = categories[i]
-        const filtresCategories = document.createElement("button")
-        const nomfiltreCategories = document.createElement("h3")
-        nomfiltreCategories.innerText = filtresGallery.name
-        filtresCategories.className ="filtres-id"
-        filtresCategories.dataset.categoryId = filtresGallery.id // Recuperation des id des categories
-        const idCategoryFiltres = document.createElement("p")
-        idCategoryFiltres.innerText = filtresGallery.id
-        divFiltres.appendChild(filtresCategories)
-        filtresCategories.appendChild(nomfiltreCategories)
+        const filtresGallery = categories[i];
+        const filtresCategories = document.createElement("button");
+        const nomfiltreCategories = document.createElement("h3");
+        nomfiltreCategories.innerText = filtresGallery.name;
+        filtresCategories.className ="filtres-id";
+        filtresCategories.dataset.categoryId = filtresGallery.id; // Recuperation des id des categories
+        const idCategoryFiltres = document.createElement("p");
+        idCategoryFiltres.innerText = filtresGallery.id;
+        divFiltres.appendChild(filtresCategories);
+        filtresCategories.appendChild(nomfiltreCategories);
 
         filtresCategories.addEventListener("click", function () {
             const worksFiltres = works.filter(function (work) {
-                return work.categoryId === parseInt(filtresCategories.dataset.categoryId) // conversion de la data pour la comparaison
-            })
-            document.querySelector(".gallery").innerHTML = ""
-            genererWorks(worksFiltres)
-        })
+                return work.categoryId === parseInt(filtresCategories.dataset.categoryId); // conversion de la data pour la comparaison
+            });
+            document.querySelector(".gallery").innerHTML = "";
+            genererWorks(worksFiltres);
+        });
     }
 }
 
-genererButton(categories) 
+genererButton(categories); 
 // fonction appelée apres categories pour avoir les filtres au-dessus
-genererWorks(works)
+genererWorks(works);
