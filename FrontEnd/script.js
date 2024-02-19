13d4abb9ee518b7ac3dac79655b21e2dfec103ff
Guillaume Destree <devstree.guillaume@gmail.com>
2024-02-19 10:48:06 +0100

    suppression code modale

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -77,28 +77,3 @@ function genererButton(categories) {
 genererButton(categories); 
 // fonction appelée apres categories pour avoir les filtres au-dessus
 genererWorks(works);
-
-    // modale Header
-    const modaleHeader = document.querySelector("header")
-    const divModaleHeader = document.createElement("div")
-    divModaleHeader.className = "modaleHeader"
-    const iconModaleHeader = document.createElement("img")
-    iconModaleHeader.src = "assets/icons/Vector.png"
-    const pModaleHeader = document.createElement("p")
-    pModaleHeader.innerText = "Mode édition"
-    header.appendChild(modaleHeader)
-    modaleHeader.appendChild(divModaleHeader)
-    divModaleHeader.appendChild(iconModaleHeader)
-    divModaleHeader.appendChild(pModaleHeader)
-    // modale Porfolio
-    const modalePorfolio = document.querySelector("#portfolio h2")
-    const divModalePorfolio = document.createElement("div")
-    divModalePorfolio.className = "modalePorfolio"
-    const iconModalePortfolio = document.createElement("img")
-    iconModalePortfolio.src = "assets/icons/Vector.png"
-    const pModalePortfolio = document.createElement("p")
-    pModalePortfolio.innerText = "modifier"
-    main.appendChild(modalePorfolio)
-    modalePorfolio.appendChild(divModalePorfolio)
-    divModalePorfolio.appendChild(iconModalePortfolio)
-    divModalePorfolio.appendChild(pModalePortfolio)
